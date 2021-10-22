e87f90e7819c94e77fdba1ee22e45cc3da54cb59
Lázaro Vanderson <eulazzo@protonmail.com>
2021-10-22 17:19:19 -0300

    Message component  animated with Moti

--- mobile_/src/components/message/index.tsx
+++ mobile_/src/components/message/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import { UserPhoto } from '../userPhoto';
+import { MotiView } from 'moti';
 
 import { styles } from './style';
 
@@ -19,7 +20,12 @@ type Props = {
 
 export const Message = ({data}:Props) =>{
   return (
-    <View style={styles.container}>
+    <MotiView 
+      from={{opacity:0,translateY:-50}}
+      animate={{opacity:1,translateY:0}}
+      transition={{type:'timing',duration:700}}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -34,6 +40,6 @@ export const Message = ({data}:Props) =>{
         </Text>
       </View>
 
-    </View>
+    </MotiView>
   );
 }
